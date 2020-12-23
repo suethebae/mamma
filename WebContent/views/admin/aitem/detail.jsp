d63c637b6af5f6ee20b7320f9550fdba016bd6ca
이인희 <ih0615@naver.com>
2020-12-23 17:39:43 +0900

    ..

---
+++ WebContent/views/admin/aitem/detail.jsp
@@ -0,0 +1,87 @@
+<%@page import="shop.mammastore.common.Parser"%>
+<%@page import="shop.mammastore.admin.vo.AitemVo"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%
+	AitemVo vo = (AitemVo) request.getAttribute("aitemVo");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script type="text/javascript">
+function modify(){
+	location.href="/aitem/modify?sq=<%=vo.getItem_sq()%>";
+	
+}
+
+function aDelete(){
+	
+	    if ( confirm('삭제하시겠습니까?')) {
+	    	location.href="aitem/delete?sq=<%=vo.getItem_sq()%>";
+		} else {
+			return;
+		}
+	}
+</script>
+</head>
+<body>
+	<!-- <form action="/aitem/amodifyProc" method="post" id="editorForm" enctype="multipare/form-data">	 -->
+	<table border=1>
+		<tr>
+			<td>상품번호</td>
+			<td><input type="text" id="nm" name="nm"
+				value=<%=vo.getItem_sq()%>></td>
+		</tr>
+		<tr>
+			<td>판매상태</td>
+			<td><%=vo.isSttus_fl()%></td>
+		</tr>
+		<%-- <tr>
+		<td>카테고리</td>
+		<td><%=vo.getCtgry_sq() %></td>
+	</tr> --%>
+		<tr>
+			<td>상품이름</td>
+			<td><%=vo.getNm()%></td>
+		</tr>
+		<tr>
+			<td>가격</td>
+			<td><%=vo.getPc()%></td>
+		</tr>
+		<tr>
+			<td>등록일자</td>
+			<td><%=vo.getDttm()%></td>
+		</tr>
+		<tr>
+			<td>재고</td>
+			<td><%=vo.getStock()%></td>
+		</tr>
+		<tr>
+			<td>이미지</td>
+			<td><%=vo.getThumb_pth()%></td>
+		</tr>
+		<tr>
+			<td>상품상세설명</td>
+			<td><%=vo.getCntnt()%></td>
+		</tr>
+	</table>
+	<button
+		onclick="location.href='/aitem/saleOn?sq=<%=vo.getItem_sq()%>'">상품
+		판매 시작</button>
+	<button
+		onclick="location.href='/aitem/saleOff?sq=<%=vo.getItem_sq()%>'">상품
+		판매 중지</button>
+	<!-- </form> -->
+	<%
+		if (!vo.isSttus_fl()) {
+	%>
+	<button onclick="modify()">수정</button>
+	<button onclick="aDelete()">삭제</button>
+	<%
+		}
+	%>
+
+</body>
+</html>
---
+++ src/shop/mammastore/admin/aitem/action/aSaleOnAction.java
@@ -0,0 +1,76 @@
+package shop.mammastore.admin.aitem.action;
+
+import static shop.mammastore.common.RegExp.REGEXP_NUMBER;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import shop.mammastore.admin.aitem.service.AitemService;
+
+import shop.mammastore.admin.vo.AitemVo;
+
+import shop.mammastore.common.Action;
+import shop.mammastore.common.ActionForward;
+import shop.mammastore.common.LoginManager;
+import shop.mammastore.common.RegExp;
+
+public class aSaleOnAction implements Action{
+@Override
+public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	HttpSession session = request.getSession();
+	LoginManager lm = LoginManager.getInstance();
+	String mngr_sq = lm.getMemberId(session);
+	
+	if(mngr_sq==null||mngr_sq.equals("")) {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>alert('잘못된 접근입니다.'); loaction.href='/'; </script>"); 
+		out.close();
+		return null;
+	}
+	
+	String ditem_sq = request.getParameter("itm_sq");
+	
+	String itm_sq = request.getParameter("sq");
+	if(!RegExp.isValidExp(itm_sq, REGEXP_NUMBER)) {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>alert('잘못된 접근입니다.'); loaction.href='/'; </script>"); 
+		out.close();
+		return null;
+	}
+	
+	
+	 AitemVo aitemVo = new AitemVo();
+	 aitemVo.setItem_sq(Integer.parseInt(itm_sq));
+	 
+	   
+	/* AitemVo aitemVo = new AitemVo(); */
+	 AitemService svc = new AitemService();
+	if (!svc.saleOn(aitemVo)) {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>alert('상품 개시 실패하였습니다..); history.back(); </script>");
+		out.close();
+		return null;
+	}
+	
+	/*
+	 * AitemVo aitemVo = svc.getAitemDetail(Integer.parseInt(itm_sq)); if
+	 * (aitemVo==null) { response.setContentType("text/html; charset=UTF-8");
+	 * PrintWriter out = response.getWriter(); out.
+	 * println("<script>alert('상품 정보 로드에 실패 했습니다.'); loaction.href='/'; </script>");
+	 * out.close(); return null; }
+	 */
+	
+	//경로설정
+	ActionForward forward = new ActionForward();
+	forward.setPath("/aitem/list");
+	return forward;
+}
+}
